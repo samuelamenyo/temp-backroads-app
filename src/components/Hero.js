684de1ca86b125acbe6158f7a0f48b7951df8fa7
Samuel <samuelamenyo30@gmail.com>
2026-05-04 13:19:38 +0000

    First Commit

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>CONTINUE YOUR EXPLORATION</h1>
+        <p>Your number One travel tour guide.</p>
+        <a href="#tours" className="btn hero-btn">
+          explore tours
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
